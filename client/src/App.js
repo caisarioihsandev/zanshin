54f7d42f6a0bef83066eb783f6e65f2b79246f4b
caisarioihsandev <iscaisario@gmail.com>
2024-06-09 01:13:52 +0700

    React Leaflet commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import 'leaflet/dist/leaflet.css';
 
-function App() {
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+import { Icon } from "leaflet";
+
+import locator from './img/gps.png';
+
+const App = () => {
+
+  // markers
+  const markers = [
+    {
+      geocode: [-6.19999, 106.80],
+      popUp: "Hello, I am pop up 1"
+    },
+    {
+      geocode: [-6.2112, 106.82],
+      popUp: "Hello, I am pop up 2"
+    },
+    {
+      geocode: [-6.215, 106.799],
+      popUp: "Hello, I am pop up 3"
+    }
+  ];
+
+  const customIcon = new Icon({
+    iconUrl: locator,
+    iconSize: [38, 38] // size of the icon
+
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer center={[-6.200000, 106.816666]} zoom={13}>
+        <TileLayer 
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+
+        {markers.map(marker => (
+          <Marker position={marker.geocode} icon={customIcon}></Marker>
+        ))}
+      </MapContainer>    
     </div>
   );
 }
